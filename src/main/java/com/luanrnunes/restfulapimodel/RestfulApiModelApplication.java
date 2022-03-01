70e66750b3277bca0a9b8285cec88dcc9b169a89
luanrnunes <luanrobertonunes@hotmail.com>
2022-02-28 23:05:49 -0300

    Project started

---
+++ src/main/java/com/luanrnunes/restfulapimodel/RestfulApiModelApplication.java
@@ -0,0 +1,13 @@
+package com.luanrnunes.restfulapimodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luanrnunes/restfulapimodel/RestfulApiModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanrnunes.restfulapimodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
