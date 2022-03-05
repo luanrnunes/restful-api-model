ef624a0222f50efb4f2c43aa7e39fb0834b3f59b
luanrnunes <luanrobertonunes@hotmail.com>
2022-03-04 23:14:35 -0300

    Mongo connection

---
+++ src/main/java/com/luanrnunes/restfulapimodel/config/Instantiation.java
@@ -0,0 +1,31 @@
+package com.luanrnunes.restfulapimodel.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.luanrnunes.restfulapimodel.domain.User;
+import com.luanrnunes.restfulapimodel.repository.UserRepository;
+
+@Configuration /*Notation para o spring saber que se trata de configuracao. EX: Inicializar os dados na base.*/
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+	}
+
+}
