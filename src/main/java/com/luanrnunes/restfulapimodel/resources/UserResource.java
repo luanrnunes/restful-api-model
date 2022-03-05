d3ca2f193f117b94d73b1c041f6d53baef06ae31
luanrnunes <luanrobertonunes@hotmail.com>
2022-03-05 12:25:19 -0300

    Resources and object changes

--- src/main/java/com/luanrnunes/restfulapimodel/resources/UserResource.java
+++ src/main/java/com/luanrnunes/restfulapimodel/resources/UserResource.java
@@ -1,7 +1,7 @@
 package com.luanrnunes.restfulapimodel.resources;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luanrnunes.restfulapimodel.domain.User;
+import com.luanrnunes.restfulapimodel.dto.UserDTO;
 import com.luanrnunes.restfulapimodel.services.UserService;
 
 @RestController  /*Notation que indica um controlador Rest*/
@@ -22,9 +23,10 @@ public class UserResource {
 	@RequestMapping(method=RequestMethod.GET) /*Notation que indica o metodo request HTTP*/
 	/*ResponseEntity e um metodo spring que ja trata diversoss erros web, basta utiliza
 	 * junto a um objeto, neste caso, a lista de usuarios*/
-	public ResponseEntity<List<User>> findAll() {
-		List<User> list = new ArrayList<>();
-		return ResponseEntity.ok().body(list);	/*Retorna com o status ok e traz a lista no corpo do retorno*/
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<User> list = service.findAll();
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);	/*Retorna com o status ok e traz a lista no corpo do retorno*/
 	}
 
 }
