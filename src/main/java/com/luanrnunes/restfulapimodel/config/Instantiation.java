2eadede56fa4216ff82ca1cdeb287b425e3b1125
luanrnunes <luanrobertonunes@hotmail.com>
2022-04-03 14:55:20 -0300

    Referencing users posts

--- src/main/java/com/luanrnunes/restfulapimodel/config/Instantiation.java
+++ src/main/java/com/luanrnunes/restfulapimodel/config/Instantiation.java
@@ -43,6 +43,9 @@ public class Instantiation implements CommandLineRunner{
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
+		eduarda.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(eduarda);
+		
 	}
 
 }
--- src/main/java/com/luanrnunes/restfulapimodel/domain/User.java
+++ src/main/java/com/luanrnunes/restfulapimodel/domain/User.java
@@ -1,9 +1,12 @@
 package com.luanrnunes.restfulapimodel.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="users")
@@ -15,6 +18,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) /*Indica que e uma referencia ao objeto*/ /*lazy = true indica que os posts só serão carregados quando eu explicitamente solicitar*/
+	private List<Post> posts = new ArrayList<>();
 	
 	public User(){
 		
@@ -49,6 +55,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
